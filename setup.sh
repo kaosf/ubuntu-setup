56a3b0ddb7c58d505573d45fb75aa2c8f36c82db
ka <ka.kaosf@gmail.com>
2012-04-06 00:52:59 +0900

    add comments for rails, SQLite3 and PostgreSQL

--- setup.sh
+++ setup.sh
@@ -22,3 +22,26 @@ rbenv install 1.9.3-p125
 rbenv global 1.9.3-p125
 gem install bundler --no-ri --no-rdoc
 rbenv rehash
+
+# install SQLite3
+sudo aptitude install sqlite3 libsqlite3-dev
+
+# install Rails with Bundler and test
+echo 'source :rubygems' >> Gemfile
+echo "gem 'rails', '3.2.2'" >> Gemfile
+bundle exec rails new sample
+cd sample
+echo "gem 'therubyracer'" >> Gemfile
+bundle install
+bundle exec rails g scaffold item name:string price:integer
+bundle exec rake db:migrate
+bundle exec rake
+bundle exec rails s
+
+# install PostgreSQL
+sudo aptitude install postgresql-8.4 postgresql-server-dev-8.4
+
+# Rails with PostgreSQL
+bundle exec rails new pg_sample -d postgresql
+cd pg_sample
+bundle install
