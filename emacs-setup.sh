abfbcb0e310916d3e88120b7c2cd1f5e75e3e146
ka <ka.kaosf@gmail.com>
2013-01-31 22:45:27 +0900

    add emacs-setup.sh

---
+++ emacs-setup.sh
@@ -0,0 +1,6 @@
+# ref. http://d.hatena.ne.jp/tomoya/20090121/1232536106
+if [ ! -d $HOME/.emacs.d/elisp ]; then
+  mkdir -p $HOME/.emacs.d/elisp
+fi
+cd $HOME/.emacs.d/elisp
+wget http://www.emacswiki.org/cgi-bin/wiki/download/install-elisp.el
