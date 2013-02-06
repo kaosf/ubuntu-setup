6ec4c9ef3061251f0a0d1cf584c80d5eaba12c11
ka <ka.kaosf@gmail.com>
2013-02-06 19:30:46 +0900

    clone all vim plugins with '--depth 1' option

--- vim-setup.sh
+++ vim-setup.sh
@@ -15,23 +15,23 @@ cd ~/.vim/bundle
 #       http://vim-users.jp/2010/11/hack185/
 #       http://vim-users.jp/2011/01/hack193/
 #       http://d.hatena.ne.jp/gabuchan/20110930/1317374566
-git clone https://github.com/Shougo/neocomplcache.git
+git clone https://github.com/Shougo/neocomplcache.git --depth 1
 # NERDtree
 #  ref. https://github.com/scrooloose/nerdtree
-git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdtree.git --depth 1
 # vim-rails
 #  ref. https://github.com/tpope/vim-rails
-git clone git://github.com/tpope/vim-rails.git
+git clone git://github.com/tpope/vim-rails.git --depth 1
 # vim-coffee-script
 #  ref. https://github.com/kchmck/vim-coffee-script/
-git clone https://github.com/kchmck/vim-coffee-script.git
+git clone https://github.com/kchmck/vim-coffee-script.git --depth 1
 # vim-ls
 #  ref. https://github.com/gkz/vim-ls
 #       http://d.hatena.ne.jp/mizchi/20120706/1341568588
-git clone git://github.com/gkz/vim-ls.git
+git clone git://github.com/gkz/vim-ls.git --depth 1
 # titanium-vim
 #  ref. http://d.hatena.ne.jp/gabuchan/20110930/1317374566
-git clone https://github.com/pekepeke/titanium-vim.git
+git clone https://github.com/pekepeke/titanium-vim.git --depth 1
 # neco-ruby-keyword-args
 #  ref. http://qiita.com/items/b8dd37efa64a9054c048
-git clone git://github.com/rhysd/neco-ruby-keyword-args.git
+git clone git://github.com/rhysd/neco-ruby-keyword-args.git --depth 1
