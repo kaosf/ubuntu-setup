c2ffc9e7051594d9b48997f7ede68011f3ba6cf1
ka <ka.kaosf@gmail.com>
2013-04-30 00:46:55 +0900

    fix ag-setup.sh

--- ag-setup.sh
+++ ag-setup.sh
@@ -2,11 +2,14 @@
 #   http://qiita.com/items/55d0c5c11a0571af3b8e
 #   http://blog.glidenote.com/blog/2013/02/28/the-silver-searcher-better-than-ack/
 
+sudo aptitude install libpcre3-dev liblzma-dev
+
 cd $HOME/local/src
-wget "https://github.com/ggreer/the_silver_searcher/archive/master.zip" --no-check-certificate
-mv master.zip the_silver_searcher-master.zip
-unzip the_silver_searcher-master.zip
-cd the_silver_searcher-master
+git clone https://github.com/ggreer/the_silver_searcher
+cd the_silver_searcher
+
+cp build.sh build.sh.bk
 sed -i -e "s/configure/configure --prefix=\$HOME\/local/g" build.sh
 ./build.sh
 make install
+mv build.sh.bk build.sh
