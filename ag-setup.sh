0d246912643214fcfb134bf4b420e28b3ce0a395
ka <ka.kaosf@gmail.com>
2013-04-30 00:51:14 +0900

    fix ag-setup.sh to use --prefix=... instead of sed

--- ag-setup.sh
+++ ag-setup.sh
@@ -8,8 +8,5 @@ cd $HOME/local/src
 git clone https://github.com/ggreer/the_silver_searcher
 cd the_silver_searcher
 
-cp build.sh build.sh.bk
-sed -i -e "s/configure/configure --prefix=\$HOME\/local/g" build.sh
-./build.sh
+./build.sh --prefix=$HOME/local
 make install
-mv build.sh.bk build.sh
