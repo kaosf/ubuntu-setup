0707c78c5e8355fb67ac3d1211f915f09b0ffb55
ka <ka.kaosf@gmail.com>
2013-06-05 15:37:20 +0900

    fix jq-setup.sh ("cd $HOME/local/src" at first)

--- jq-setup.sh
+++ jq-setup.sh
@@ -2,6 +2,7 @@
 #   http://qiita.com/items/10d9c15d20390dfde310#comment-7851627e2c5536c8465b
 #   http://stedolan.github.io/jq/download/
 
+cd $HOME/local/src
 git clone https://github.com/stedolan/jq.git
 cd jq
 autoreconf
