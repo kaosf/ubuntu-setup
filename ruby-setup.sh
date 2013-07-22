a107a93b2a21390b4f8e7609dcba35a46a3b2947
ka <ka.kaosf@gmail.com>
2013-07-23 00:48:39 +0900

    modify ruby-setup.sh

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -3,16 +3,14 @@
 #   https://github.com/sstephenson/rbenv
 #   https://github.com/sstephenson/ruby-build
 sudo aptitude install zlib1g-dev libssl-dev libreadline6-dev
-git clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
+git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshenv
 echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv
 exec $SHELL -l
 mkdir -p $HOME/.rbenv/plugins
-cd $HOME/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
-cd
-rbenv install 2.0.0-p195
-rbenv global 2.0.0-p195
+git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+rbenv install 2.0.0-p247
+rbenv global 2.0.0-p247
 gem install bundler --no-ri --no-rdoc
 rbenv rehash
 
