6e3cbbafdeae48dab696354629a19b8dea2a523b
ka <ka.kaosf@gmail.com>
2013-09-08 02:27:38 +0900

    modify emacs-setup.sh for Desktop edition
    this is also enable for Server edition

--- emacs-setup.sh
+++ emacs-setup.sh
@@ -4,6 +4,6 @@ cd $HOME/local/src
 wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-24.3.tar.gz
 tar xzf emacs-24.3.tar.gz
 cd emacs-24.3
-./configure --prefix=$HOME/local --with-x-toolkit=no
+./configure --prefix=$HOME/local --without-x
 make
 make install
