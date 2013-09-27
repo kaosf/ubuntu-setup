f296fedc9750ce80a7c12f2041d9490b454d8885
ka <ka.kaosf@gmail.com>
2013-09-28 02:20:22 +0900

    remove downloaded "lpm" forcedly

--- lpm-setup.sh
+++ lpm-setup.sh
@@ -1,7 +1,7 @@
 wget http://www.kasahara.ws/lpm/lpm
 chmod +x lpm
 ./lpm initlocaldir --local=.lcl
-rm lpm
+rm lpm -f
 
 tail -3 .zshrc >> .zshenv
 head -$(($(cat .zshrc | wc -l) - 3)) .zshrc | tee .zshrc > /dev/null
