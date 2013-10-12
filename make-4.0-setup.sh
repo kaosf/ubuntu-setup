4fe7aec074363d2985831d4ade1db55c110df319
ka <ka.kaosf@gmail.com>
2013-10-13 01:21:17 +0900

    install guile-2.0 for using it on make-4.0

--- make-4.0-setup.sh
+++ make-4.0-setup.sh
@@ -1,5 +1,7 @@
 # ref. http://www.gnu.org/software/make/
 
+sudo aptitude install guile-2.0 guile-2.0-dev
+
 cd $HOME/local/src
 wget http://ftp.gnu.org/gnu/make/make-4.0.tar.gz
 tar xzf make-4.0.tar.gz
