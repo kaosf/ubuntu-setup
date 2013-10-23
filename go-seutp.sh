53d03335f5374b054601bfc6b1859edffa1a0398
ka <ka.kaosf@gmail.com>
2013-10-23 21:43:32 +0900

    create go-setup.sh

---
+++ go-seutp.sh
@@ -0,0 +1,14 @@
+# ref. http://golang.org/doc/install
+wget http://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz
+tar xzf go1.1.2.linux-amd64.tar.gz
+mv go $HOME/.go
+echo 'export GOROOT=$HOME/.go' >> .zshenv
+echo 'export PATH=$PATH:$GOROOT/bin' >> .zshenv
+echo 'export GOPATH=$HOME/.gocode' >> .zshenv
+echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
+source $HOME/.zshrc
+
+# ref. https://github.com/heroku/hk
+go get github.com/heroku/hk
+echo 'machine api.heroku.com login <email> password <password>' > $HOME/.netrc
+chmod 600 $HOME/.netrc
