7c2e54c632d17e388b2e227e04cb74415614e394
ka <ka.kaosf@gmail.com>
2013-10-25 15:54:10 +0900

    add commands to install Node.js manually

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -9,3 +9,12 @@ export PATH=$HOME/.nodebrew/current/bin:$PATH
 # nodebrew ls-all
 nodebrew install-binary 0.10
 nodebrew use latest
+
+## install Node.js manually
+#cd $HOME/local/src
+#wget http://nodejs.org/dist/v0.10.21/node-v0.10.21.tar.gz
+#tar xzf node-v0.10.21.tar.gz
+#cd node-v0.10.21
+#./configure --prefix=$HOME/local --dest-os=linux
+#make
+#make install
