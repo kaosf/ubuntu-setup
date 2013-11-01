3fe965c988f0338e33dbd07c66cfacf916ff3a27
ka <ka.kaosf@gmail.com>
2013-11-01 17:09:28 +0900

    add packages for Ruby

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -2,7 +2,16 @@
 # ref.
 #   https://github.com/sstephenson/rbenv
 #   https://github.com/sstephenson/ruby-build
-sudo aptitude install zlib1g-dev libssl-dev libreadline6-dev
+
+# zlib1g-dev libssl-dev libreadline6-dev are needed to build
+# libsqlite3-dev for SQLite3
+# postgresql-server-dev-9.1 for PostgreSQL 9.1
+# libxslt1-dev libxml2-dev for nokogiri
+sudo aptitude install zlib1g-dev libssl-dev libreadline6-dev \
+  libsqlite3-dev \
+  postgresql-server-dev-9.1 \
+  libxslt1-dev libxml2-dev
+
 git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshenv
 echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv
@@ -13,7 +22,3 @@ rbenv install 2.0.0-p247
 rbenv global 2.0.0-p247
 gem install bundler --no-ri --no-rdoc
 rbenv rehash
-
-# to install nokogiri
-sudo aptitude install libxslt1-dev libxml2-dev
-gem install nokogiri
