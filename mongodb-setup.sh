410ffb4f4cb4a0e5ec8784aa9a1201e410625f3d
ka <ka.kaosf@gmail.com>
2013-11-07 14:10:53 +0900

    modify "sudo apt-key ..." command
    ref. http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/

--- mongodb-setup.sh
+++ mongodb-setup.sh
@@ -1,5 +1,5 @@
 # ref. http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-key adv --keyserver keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
 # ref. http://d.hatena.ne.jp/yutakikuchi/20111013/1318436872
