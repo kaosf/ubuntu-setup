f23ec4fa79487d383b64902efdae662dc4602ac6
ka <ka.kaosf@gmail.com>
2013-11-07 14:10:20 +0900

    remove expression comments from mongodb-setup.sh

--- mongodb-setup.sh
+++ mongodb-setup.sh
@@ -2,8 +2,6 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
-# echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list #=> error (permission denied)
-# sudo sh -c 'echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list'
 # ref. http://d.hatena.ne.jp/yutakikuchi/20111013/1318436872
 
 # sudo vim /etc/apt/sources.list.d/10gen.list
