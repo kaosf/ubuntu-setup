be81854e92eddf1392f4c802e7bcd8c36aa8be2d
ka <ka.kaosf@gmail.com>
2013-11-07 14:11:50 +0900

    remove comments for using vim to configure from mongodb-setup.sh

--- mongodb-setup.sh
+++ mongodb-setup.sh
@@ -4,9 +4,6 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
 # ref. http://d.hatena.ne.jp/yutakikuchi/20111013/1318436872
 
-# sudo vim /etc/apt/sources.list.d/10gen.list
-# + deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen
-
 sudo aptitude update
 sudo aptitude install mongodb-10gen
 
