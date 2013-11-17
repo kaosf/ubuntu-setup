c9216d2363373e4a5a96b7c1bff19e586de12489
ka <ka.kaosf@gmail.com>
2013-11-17 16:02:44 +0900

    fix order of commands in jq-setup.sh

--- jq-setup.sh
+++ jq-setup.sh
@@ -2,12 +2,6 @@
 #   http://qiita.com/items/10d9c15d20390dfde310#comment-7851627e2c5536c8465b
 #   http://stedolan.github.io/jq/download/
 
-cd $HOME/local/src
-git clone https://github.com/stedolan/jq.git
-cd jq
-autoreconf -i
-./configure --prefix=$HOME/local
-
 # `flex` package and `yacc` command are needed for make
 # ref. http://diaryrnsit.blogspot.jp/2012/10/how-to-install-lex-in-ubuntu-12.html
 sudo aptitude install flex bison
@@ -15,5 +9,10 @@ sudo aptitude install flex bison
 # ref. http://manpages.ubuntu.com/manpages/hardy/man1/libtoolize.1.html
 sudo aptitude install libtool automake
 
+cd $HOME/local/src
+git clone https://github.com/stedolan/jq.git
+cd jq
+autoreconf -i
+./configure --prefix=$HOME/local
 make
 make install
