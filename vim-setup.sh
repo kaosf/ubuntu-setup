23126d3cddfc383f7eb1dc9869ed6e0b6ad115c4
ka <ka.kaosf@gmail.com>
2013-12-30 16:13:27 +0900

    modify "~" to "$HOME" in vim-setup.sh

--- vim-setup.sh
+++ vim-setup.sh
@@ -3,6 +3,6 @@ if [ ! -d $HOME/.vim/bundle ]
 then
   mkdir -p $HOME/.vim/bundle
 fi
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
 
 # execute :NeoBundleInstall in Vim command mode
