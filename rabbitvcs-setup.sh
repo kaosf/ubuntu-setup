226d487accf1add2fafbce7b08ade7f783875e32
ka <ka.kaosf@gmail.com>
2014-01-10 00:27:47 +0900

    remove rabbitvcs-setup.sh

--- rabbitvcs-setup.sh
+++
@@ -1,9 +0,0 @@
-sudo add-apt-repository ppa:rabbitvcs/ppa
-echo "deb http://ppa.launchpad.net/rabbitvcs/ppa/ubuntu precise main" | sudo tee -a /etc/apt/sources.list
-sudo aptitude update
-sudo aptitude install rabbitvcs-cli rabbitvcs-nautilus3 rabbitvcs-gedit
-
-# ref.
-#   http://wiki.rabbitvcs.org/wiki/install/ubuntu
-#   http://richardjh.org/blog/install-rabbitvcs-on-ubuntu-12.04/
-#   http://nouzui2007.blogspot.jp/2011/09/tortoisesvnubuntu.html
