0146781075097907c64661786e9ad2d43a435b2d
ka <ka.kaosf@gmail.com>
2014-02-13 17:04:36 +0900

    fix go-setup.sh
    .zshenv -> $HOME/.zshenv
    remove the archive file

--- go-seutp.sh
+++ go-seutp.sh
@@ -2,11 +2,12 @@
 wget http://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz
 tar xzf go1.1.2.linux-amd64.tar.gz
 mv go $HOME/.go
-echo 'export GOROOT=$HOME/.go' >> .zshenv
-echo 'export PATH=$PATH:$GOROOT/bin' >> .zshenv
-echo 'export GOPATH=$HOME/.gocode' >> .zshenv
+echo 'export GOROOT=$HOME/.go' >> $HOME/.zshenv
+echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.zshenv
+echo 'export GOPATH=$HOME/.gocode' >> $HOME/.zshenv
 echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
 source $HOME/.zshenv
+rm go1.1.2.linux-amd64.tar.gz
 
 # ref. https://github.com/heroku/hk
 # Mercurial ("hg" command) is required
