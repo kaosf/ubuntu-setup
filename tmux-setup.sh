d7f53671900a822186c2cbb2598d29f2509954a0
ka <ka.kaosf@gmail.com>
2014-03-30 11:15:42 +0900

    update tmux-setup for tmux 1.9a

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -2,9 +2,9 @@ sudo aptitude install libevent-dev libncurses-dev
 #sudo aptitude install libevent-dev libncurses5-dev
 
 cd $HOME/local/src
-wget http://optimate.dl.sourceforge.net/project/tmux/tmux/tmux-1.8/
-tar xzf tmux-1.8.tar.gz
-cd tmux-1.8
+wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz
+tar xzf tmux-1.9a.tar.gz
+cd tmux-1.9a
 ./configure --prefix=$HOME/local
 make
 make install
