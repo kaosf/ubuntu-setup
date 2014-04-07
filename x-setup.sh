ffc7285cde770921cc1b7fbe8cef90811a3b87a8
ka <ka.kaosf@gmail.com>
2014-04-08 00:52:34 +0900

    Modify x-setup for notify-send command

--- x-setup.sh
+++ x-setup.sh
@@ -2,3 +2,7 @@ sudo aptitude install firefox ibus-mozc git-gui xsel
 
 # VNC Client
 sudo aptitude install xtightvncviewer
+
+# Desktop Notification
+# ref. http://ubuntuforums.org/showthread.php?t=2121753
+sudo aptitude install libnotify-bin
