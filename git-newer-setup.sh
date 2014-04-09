3f93f129db51c8fd15efe10ba53d056a3f9c4ff3
ka <ka.kaosf@gmail.com>
2014-04-10 03:15:36 +0900

    Create a shellscript to install newer version git

---
+++ git-newer-setup.sh
@@ -0,0 +1,4 @@
+# ref. http://stackoverflow.com/questions/19109542/installing-latest-version-of-git-in-ubuntu
+sudo add-apt-repository ppa:git-core/ppa
+sudo aptitude update
+sudo aptitude install git
