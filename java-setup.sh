8b2d1c65fc0aa5495299471f6418e8cdaef88fe5
ka <ka.kaosf@gmail.com>
2014-05-01 01:05:08 +0900

    Fix the comment for Oracle Java 7

--- java-setup.sh
+++ java-setup.sh
@@ -1,7 +1,7 @@
 # Java 7
 sudo aptitude install openjdk-7-jdk
 
-# Oracle Java 6
+# Oracle Java 7
 # ref. http://qiita.com/ka_/items/dbeca6fac63a6e8ec9a6
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
