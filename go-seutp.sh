4efcf55347488666a3edbee64861d6a87ba8df5a
ka <ka.kaosf@gmail.com>
2014-05-04 17:47:12 +0900

    Modify the command for installation "hk"
    Ref. https://github.com/heroku/hk/tree/4e01c29331ad0d1ff3f7e00feddfe782ce7b5734#mac-os-x-linux-bsd
    
    Remove the mercurial installation and the guide of "hk login"

--- go-seutp.sh
+++ go-seutp.sh
@@ -9,8 +9,5 @@ echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
 source $HOME/.zshenv
 rm -f go1.2.1.linux-amd64.tar.gz
 
-# ref. https://github.com/heroku/hk
-# Mercurial ("hg" command) is required
-sudo aptitude install mercurial
-go get github.com/heroku/hk
-hk login # or "heroku login"
+# ref. https://github.com/heroku/hk#mac-os-x-linux-bsd
+L=$HOME/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
