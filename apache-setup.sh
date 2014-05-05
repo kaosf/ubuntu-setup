8fa4d91d5085a7d69e60d3c0e2503eb52ade77c0
ka <ka.kaosf@gmail.com>
2014-05-05 22:46:32 +0900

    Modify apache-setup.sh

--- apache-setup.sh
+++ apache-setup.sh
@@ -1,18 +1,7 @@
-# Apache2
 sudo aptitude install apache2
 sudo a2enmod rewrite
+sudo a2enmod userdir
 sudo vim /etc/apache2/site-available/default
-...
-- 	DocumentRoot /var/www
-+ 	DocumentRoot /home/ka/www
-...
-- 	<Directory /var/www/>
-- 		Options Indexes FollowSymLinks MultiViews
-- 		AllowOverride None
-+ 	<Directory /home/ka/www/>
-+ 		Options ExecCGI Indexes FollowSymLinks MultiViews
-+ 		AllowOverride All
-...
-
-mkdir $HOME/www
-sudo service apache2 restart
+# Add "ExecCGI" for CGI
+#- 		Options Indexes FollowSymLinks MultiViews
+#+ 		Options ExecCGI Indexes FollowSymLinks MultiViews
