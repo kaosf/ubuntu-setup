7f3752c1c5bc6f07a37253d923bcb4120074be0a
ka <ka.kaosf@gmail.com>
2014-05-15 00:06:14 +0900

    Modify ag-setup.sh

--- ag-setup.sh
+++ ag-setup.sh
@@ -2,8 +2,9 @@
 #   http://qiita.com/items/55d0c5c11a0571af3b8e
 #   http://blog.glidenote.com/blog/2013/02/28/the-silver-searcher-better-than-ack/
 
-sudo aptitude install automake libpcre3-dev liblzma-dev pkg-config
+sudo aptitude install automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
 
+mkdir -p $HOME/local/src
 cd $HOME/local/src
 git clone https://github.com/ggreer/the_silver_searcher
 cd the_silver_searcher
