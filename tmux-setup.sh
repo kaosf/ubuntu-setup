5848292305af08b645c794c611e04387bc69202c
ka <ka.kaosf@gmail.com>
2014-05-15 00:36:33 +0900

    Modify tmux-setup.sh

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -4,6 +4,7 @@ sudo aptitude install libevent-dev libncurses-dev
 cd $HOME/local/src
 wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz
 tar xzf tmux-1.9a.tar.gz
+rm -f tmux-1.9a.tar.gz
 cd tmux-1.9a
 ./configure --prefix=$HOME/local
 make
