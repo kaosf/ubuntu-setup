31d88d67e269f8c89e1be476c04b9bb322aff960
ka <ka.kaosf@gmail.com>
2014-05-15 11:44:17 +0900

    Create tig-setup.sh

---
+++ tig-setup.sh
@@ -0,0 +1,12 @@
+# ref. https://github.com/jonas/tig/issues/39#issuecomment-4992269
+sudo aptitude install automake libncurses5-dev libncursesw5-dev
+mkdir -p $HOME/local/src
+cd $HOME/local/src
+git clone https://github.com/jonas/tig
+cd tig
+git checkout tig-2.0.2
+./autogen.sh
+./configure --prefix=$HOME/local
+make
+make install
+git checkout master
