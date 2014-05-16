75d0fb715fe5d64a6772c74bdd30753d4ac7aa31
ka <ka.kaosf@gmail.com>
2014-05-16 17:00:51 +0900

    Update ruby-setup
    Install Ruby 2.1.2.
    The error on 2.1.1 has been resolved, so remove some codes for it.
    Discard old Ubuntu 12.04.

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -3,14 +3,12 @@
 #   https://github.com/sstephenson/rbenv
 #   https://github.com/sstephenson/ruby-build
 #   http://qiita.com/ryurock/items/77542e00cbca5268e323
-#   https://github.com/sstephenson/ruby-build/issues/526#issuecomment-37933242
 
-# curl is needed for patch
 # zlib1g-dev libssl-dev libreadline6-dev are needed to build
 # libsqlite3-dev for SQLite3
 # postgresql-server-dev-9.3 for PostgreSQL 9.3
 # libxslt1-dev libxml2-dev for nokogiri
-sudo aptitude install curl zlib1g-dev libssl-dev libreadline6-dev \
+sudo aptitude install zlib1g-dev libssl-dev libreadline6-dev \
   libsqlite3-dev \
   postgresql-server-dev-9.3 \
   libxslt1-dev libxml2-dev
@@ -22,9 +20,8 @@ exec $SHELL -l
 mkdir -p $HOME/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 git clone https://github.com/ianheggie/rbenv-binstubs.git $HOME/.rbenv/plugins/rbenv-binstubs
-#rbenv install 2.1.1 #=> Error
-curl -fsSL https://gist.github.com/mislav/a18b9d7f0dc5b9efc162.txt | rbenv install --patch 2.1.1
-rbenv global 2.1.1
+rbenv install 2.1.2
+rbenv global 2.1.2
 rbenv rehash
 gem install bundler --no-ri --no-rdoc
 rbenv rehash
