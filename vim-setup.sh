7dc98bf0945b39c434bc0269fdfd175449453675
ka <ka.kaosf@gmail.com>
2014-05-16 16:22:33 +0900

    Remove another repository vim installation

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,9 +1,3 @@
-# install newer Vim
-# ref. http://linuxg.net/how-to-install-vim-7-4-on-ubuntu-13-10-13-04-12-04-linux-mint-16-15-13-and-debian-sid/
-sudo add-apt-repository ppa:fcwu-tw/ppa
-sudo aptitude update
-sudo aptitude install vim
-
 # for +lua +clipboard
 sudo aptitude install vim-gnome
 
