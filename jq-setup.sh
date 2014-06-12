4500b338e4e64219f1c321f11f3688b85a6bac6e
ka <ka.kaosf@gmail.com>
2014-06-12 20:38:39 +0900

    Remove manually installation commands

--- jq-setup.sh
+++ jq-setup.sh
@@ -1,15 +1,1 @@
-# ref.
-#   http://qiita.com/items/10d9c15d20390dfde310#comment-7851627e2c5536c8465b
-#   http://stedolan.github.io/jq/download/
-
-# `flex` package and `yacc` command are needed for make
-# ref. http://diaryrnsit.blogspot.jp/2012/10/how-to-install-lex-in-ubuntu-12.html
-sudo aptitude install flex bison
-
-cd $HOME/local/src
-git clone https://github.com/stedolan/jq.git
-cd jq
-autoreconf -i
-./configure --prefix=$HOME/local
-make
-make install
+sudo aptitude install jq
