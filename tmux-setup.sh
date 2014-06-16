908f96c5172e02b006c53419189efd7a2685f1ec
ka <ka.kaosf@gmail.com>
2014-06-16 23:10:19 +0900

    Move "xsel" installation command from tmux-setup.sh to x-setup.sh

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -13,9 +13,3 @@ cd tmux-1.9a
 ./configure --prefix=$HOME/local
 make
 make install
-
-# for a combination with clipboard
-# ref.
-#   http://www.mk-mode.com/octopress/2013/06/22/linux-mint-tmux-clipboard-share/
-#   http://www.mk-mode.com/octopress/2013/06/21/linux-mint-install-xsel/
-sudo aptitude install xsel
--- x-setup.sh
+++ x-setup.sh
@@ -6,3 +6,9 @@ sudo aptitude install xtightvncviewer
 # Desktop Notification
 # ref. http://ubuntuforums.org/showthread.php?t=2121753
 sudo aptitude install libnotify-bin
+
+# for a combination of tmux and clipboard
+# ref.
+#   http://www.mk-mode.com/octopress/2013/06/22/linux-mint-tmux-clipboard-share/
+#   http://www.mk-mode.com/octopress/2013/06/21/linux-mint-install-xsel/
+sudo aptitude install xsel
