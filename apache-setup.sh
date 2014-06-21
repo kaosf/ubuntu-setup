30a118b417c013eb2b72bd568c2a60d74b72c8e0
ka <ka.kaosf@gmail.com>
2014-06-21 22:48:57 +0900

    Use reverse proxy for homepage-powered-by-ring
    ref.
      http://httpd.apache.org/docs/trunk/mod/mod_authz_core.html#require
      http://httpd.apache.org/docs/2.4/en/mod/mod_proxy.html#proxy
      https://github.com/kaosf/homepage-powered-by-ring

--- apache-setup.sh
+++ apache-setup.sh
@@ -1,7 +1,18 @@
 sudo aptitude install apache2
-sudo a2enmod rewrite
-sudo a2enmod userdir
-sudo vim /etc/apache2/site-available/default
-# Add "ExecCGI" for CGI
-#- 		Options Indexes FollowSymLinks MultiViews
-#+ 		Options ExecCGI Indexes FollowSymLinks MultiViews
+cat <<EOF | sudo tee /etc/apache2/sites-available/default.conf
+<VirtualHost *:80>
+  ServerName kaosfield.net
+
+  ProxyRequests Off
+  <Proxy *>
+    Require all granted
+  </Proxy>
+
+  ProxyPass / http://ka-ring.herokuapp.com/
+  ProxyPassReverse / http://ka-ring.herokuapp.com/
+</VirtualHost>
+EOF
+sudo a2enmod proxy_http
+sudo a2dissite 000-default
+sudo a2ensite default
+sudo service apache2 restart
