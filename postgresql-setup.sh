50893003c9d31f97985fbedb6ac6f38ad9ca99f4
ka <ka.kaosf@gmail.com>
2014-06-23 00:00:20 +0900

    Avoid the locale error on PostgreSQL installation
    ref. http://stackoverflow.com/questions/17399622/postgresql-9-2-installation-on-ubuntu-12-04/20137471#20137471

--- postgresql-setup.sh
+++ postgresql-setup.sh
@@ -1,1 +1,6 @@
+# To avoid the following error
+#Error: The locale requested by the environment is invalid.
+echo 'LC_ALL="en_US.UTF-8"' | sudo tee -a /etc/environment
+
 sudo aptitude install postgresql-9.3 postgresql-server-dev-9.3
+# ref. http://stackoverflow.com/questions/17399622/postgresql-9-2-installation-on-ubuntu-12-04/20137471#20137471
