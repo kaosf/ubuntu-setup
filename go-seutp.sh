edb6994b4068dd2107228b3cd8c66fe5c1cb8430
ka <ka.kaosf@gmail.com>
2014-06-22 22:28:09 +0900

    Upgrade Go version to 1.3

--- go-seutp.sh
+++ go-seutp.sh
@@ -1,13 +1,14 @@
 # ref. http://golang.org/doc/install
-wget http://go.googlecode.com/files/go1.2.1.linux-amd64.tar.gz
-tar xzf go1.2.1.linux-amd64.tar.gz
+VERSION=1.3
+wget http://go.googlecode.com/files/go${VERSION}.linux-amd64.tar.gz
+tar xzf go${VERSION}.linux-amd64.tar.gz
 mv go $HOME/.go
 echo 'export GOROOT=$HOME/.go' >> $HOME/.zshenv
 echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.zshenv
 echo 'export GOPATH=$HOME/.gocode' >> $HOME/.zshenv
 echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
 source $HOME/.zshenv
-rm -f go1.2.1.linux-amd64.tar.gz
+rm -f go${VERSION}.linux-amd64.tar.gz
 
 # ref. https://github.com/heroku/hk#mac-os-x-linux-bsd
 L=$HOME/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
