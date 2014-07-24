e4e017be8117007ff1337155856c15e61602b441
ka <ka.kaosf@gmail.com>
2014-07-24 14:05:44 +0900

    Install peco

--- go-seutp.sh
+++ go-seutp.sh
@@ -12,3 +12,6 @@ rm -f go${VERSION}.linux-amd64.tar.gz
 
 # ref. https://github.com/heroku/hk#mac-os-x-linux-bsd
 L=$HOME/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
+
+# ref. https://github.com/peco/peco#go-get
+go get github.com/peco/peco/cmd/peco
