197ac2a1c22e4df3249fffb044c0a124044c3688
ka <ka.kaosf@gmail.com>
2014-08-05 21:03:15 +0900

    Fix ag-setup.sh

--- ag-setup.sh
+++ ag-setup.sh
@@ -1,13 +1,1 @@
-# ref.
-#   http://qiita.com/items/55d0c5c11a0571af3b8e
-#   http://blog.glidenote.com/blog/2013/02/28/the-silver-searcher-better-than-ack/
-
-sudo aptitude install automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-
-mkdir -p $HOME/local/src
-cd $HOME/local/src
-git clone https://github.com/ggreer/the_silver_searcher
-cd the_silver_searcher
-
-./build.sh --prefix=$HOME/local
-make install
+sudo aptitude install silversearcher-ag
