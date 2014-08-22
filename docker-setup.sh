c303ddc600426905b581eee7de3082830639522c
ka <ka.kaosf@gmail.com>
2014-08-22 19:34:59 +0900

    Install version 1.2.0

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,6 +1,6 @@
 # ref. http://askubuntu.com/a/473720
-# Install version 0.11.1
-sudo add-apt-repository ppa:docker-maint/testing
+# Install version 1.2.0
+wget -qO- https://get.docker.io/gpg | sudo apt-key add -
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo aptitude update
-sudo aptitude install docker.io
-sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo aptitude install lxc-docker
