03b450b1fa0a80f0b488793a1fd0a70d89e4b93a
ka <ka.kaosf@gmail.com>
2014-08-22 19:34:20 +0900

    Install version 0.11.1

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,3 +1,6 @@
-# Install version 0.9.1~dfsg1-2
+# ref. http://askubuntu.com/a/473720
+# Install version 0.11.1
+sudo add-apt-repository ppa:docker-maint/testing
+sudo aptitude update
 sudo aptitude install docker.io
 sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
