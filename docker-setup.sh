55a8b0adbbd9825494681aaa27b48b08f3932485
ka <ka.kaosf@gmail.com>
2014-08-30 03:58:04 +0900

    Explicate the version of Docker (lxc-docker package)

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,6 +1,5 @@
 # ref. http://askubuntu.com/a/473720
-# Install version 1.2.0
 wget -qO- https://get.docker.io/gpg | sudo apt-key add -
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo aptitude update
-sudo aptitude install lxc-docker
+sudo aptitude install lxc-docker-1.2.0
