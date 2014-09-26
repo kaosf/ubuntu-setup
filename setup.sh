c02def8f01e81b9f1113cb312fe33c90e28e9bd6
ka <ka.kaosf@gmail.com>
2014-09-26 17:17:52 +0900

    Add comments

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 sudo apt-get -y -q update
 sudo apt-get -y -q upgrade
+# Force to fix locale configurations to "en_US.UTF-8"
 sudo locale-gen en_US en_US.UTF-8
 sudo dpkg-reconfigure locales
 echo 'LC_ALL=en_US.UTF-8' | sudo tee -a /etc/environment
 echo 'LANG=en_US.UTF-8'   | sudo tee -a /etc/environment
 sudo reboot
+# Some packages needed at least
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get -y update
