58898591c3748d1a13ca4d2ca89ec826299a7c16
ka <ka.kaosf@gmail.com>
2014-10-20 00:25:58 +0900

    Update mongodb-setup.sh

--- mongodb-setup.sh
+++ mongodb-setup.sh
@@ -1,11 +1,11 @@
 # ref. http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
-sudo apt-key adv --keyserver keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
-echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 # ref. http://d.hatena.ne.jp/yutakikuchi/20111013/1318436872
 
 sudo aptitude update
-sudo aptitude install mongodb-10gen
+sudo aptitude install -y mongodb-org
 
 # ref. http://te2u.hatenablog.jp/entry/2013/02/06/222342
 echo "smallfiles = true" | sudo tee -a /etc/mongodb.conf
