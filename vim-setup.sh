9f67e0d709bce83b05c094be78ffcc00e99cde90
ka <ka.kaosf@gmail.com>
2014-11-17 23:35:32 +0900

    Add commands for building Vim by myself
    Thanks to @mizoki.

--- vim-setup.sh
+++ vim-setup.sh
@@ -20,3 +20,53 @@ git clone git://github.com/kaosf/ka-vim-snippets.git $HOME/.vim/ka-vim-snippets
 # setup vimproc
 cd $HOME/.vim/bundle/vimproc
 make
+
+## Build by myself
+mkdir -p $HOME/local/src
+cd $HOME/local/src
+# ref. https://github.com/vim-jp/vim
+git clone https://github.com/vim-jp/vim
+cd vim
+./configure \
+  --prefix=$HOME/local \
+  --enable-gui=gtk2 \
+  --enable-perlinterp=yes \
+  --enable-pythoninterp=yes \
+  --enable-rubyinterp \
+  --enable-luainterp=yes \
+  --enable-multibyte \
+  --enable-fail-if-missing \
+  --with-features=huge \
+  --with-x \
+  --with-luajit
+
+# To resolve the error below;
+#
+#     configure: error: could not configure lua
+sudo aptitude install lua5.2 liblua5.2-dev luajit libluajit-5.1-2 libluajit-5.1-dev
+
+# To resolve the error below;
+#
+#     configure: error: could not configure perl
+sudo aptitude install libperl-dev
+
+# To resolve the error below;
+#
+#     can't find it!
+#     configure: error: could not configure python
+sudo aptitude install python-dev python3-dev
+
+./configure \
+  --prefix=$HOME/local \
+  --enable-gui=gtk2 \
+  --enable-perlinterp=yes \
+  --enable-pythoninterp=yes \
+  --enable-rubyinterp \
+  --enable-luainterp=yes \
+  --enable-multibyte \
+  --enable-fail-if-missing \
+  --with-features=huge \
+  --with-x \
+  --with-luajit
+make
+make install
