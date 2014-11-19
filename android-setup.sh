7c7b50719e3b9bd974a84d1a0bcdf37a04fe0c1a
ka <ka.kaosf@gmail.com>
2014-11-19 20:42:19 +0900

    Upgrade Android SDK Manager version

--- android-setup.sh
+++ android-setup.sh
@@ -8,8 +8,8 @@ then
   mkdir $HOME/local/opt
 fi
 cd $HOME/local
-wget http://dl.google.com/android/android-sdk_r22.6.2-linux.tgz
-tar xf android-sdk_r22.6.2-linux.tgz
+wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
+tar xf android-sdk_r23.0.2-linux.tgz
 
 echo 'export PATH=$HOME/local/android-sdk-linux/tools:$PATH' >> $HOME/.zshenv
 echo 'export PATH=$HOME/local/android-sdk-linux/platform-tools:$PATH' >> $HOME/.zshenv
