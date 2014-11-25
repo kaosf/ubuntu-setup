3a30894e15b27aa3efc6931f1f64cac7979ef1f1
ka <ka.kaosf@gmail.com>
2014-11-25 12:10:32 +0900

    Fix linuxbrew-setup.sh
    Add a comment about press Enter

--- linuxbrew-setup.sh
+++ linuxbrew-setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 
 ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
+# Press Enter
 echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"'                >> $HOME/.zshenv
 echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"'    >> $HOME/.zshenv
 echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> $HOME/.zshenv
