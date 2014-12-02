f18ae428466d75e890a041d196e3211a3a1e45ad
ka <ka.kaosf@gmail.com>
2014-12-02 23:47:49 +0900

    Install golang by Linuxbrew

--- go-setup.sh
+++ go-setup.sh
@@ -1,14 +1,11 @@
 # ref. http://golang.org/doc/install
-VERSION=1.3
-wget http://golang.org/dl/go${VERSION}.linux-amd64.tar.gz
-tar xzf go${VERSION}.linux-amd64.tar.gz
-mv go $HOME/.go
-echo 'export GOROOT=$HOME/.go' >> $HOME/.zshenv
-echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.zshenv
-echo 'export GOPATH=$HOME/.gocode' >> $HOME/.zshenv
-echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
+brew install go
+cat <<EOS >> $HOME/.zshenv
+export PATH=\$HOME/.linuxbrew/opt/go/libexec/bin:\$PATH
+export GOPATH=\$HOME/.gocode
+export PATH=\$HOME/.gocode/bin:\$PATH
+EOS
 source $HOME/.zshenv
-rm -f go${VERSION}.linux-amd64.tar.gz
 
 # ref. https://github.com/heroku/hk#mac-os-x-linux-bsd
 L=$HOME/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
