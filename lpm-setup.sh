abb014916e08c8f56104f826d05998a8bcbc09c2
ka <ka.kaosf@gmail.com>
2015-01-21 01:02:20 +0900

    Remove lpm-setup.sh

--- lpm-setup.sh
+++
@@ -1,9 +0,0 @@
-wget http://www.kasahara.ws/lpm/lpm
-chmod +x lpm
-./lpm initlocaldir --local=.lcl
-rm lpm -f
-
-tail -3 .zshrc >> .zshenv
-head -$(($(cat .zshrc | wc -l) - 3)) .zshrc | tee .zshrc > /dev/null
-
-# ref. http://www.kasahara.ws/lpm/quickstart_ja.html
