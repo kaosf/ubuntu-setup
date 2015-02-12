c65a71d554efe6838ab0caf0239270a543d10212
ka <ka.kaosf@gmail.com>
2015-02-12 13:39:52 +0900

    Revert "Install Nodebrew by Linuxbrew"
    This reverts commit a4609c98df4a8af4571a9fd26332a8cbe4df212c.

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -1,10 +1,9 @@
 # install Node.js with nodebrew
 # ref. https://github.com/hokaccha/nodebrew
 
-cat <<EOS >> $HOME/.zshenv
-export PATH=\$HOME/.nodebrew/current/bin:\$PATH
-export NODEBREW_ROOT=\$HOME/.linuxbrew/var/nodebrew
-EOS
+curl -L git.io/nodebrew | perl - setup
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> $HOME/.zshenv
+export PATH=$HOME/.nodebrew/current/bin:$PATH
 
 # nodebrew help
 # nodebrew ls-all
