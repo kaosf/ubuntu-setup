fb5012d30771fa3bed5e9c18a11e58200df4a5a3
ka <ka.kaosf@gmail.com>
2015-04-14 14:35:10 +0900

    Remove notes of Linux kernel 3.19.3

--- update-kernel.sh
+++ update-kernel.sh
@@ -1,11 +1,3 @@
-# http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3.19.3-031903_3.19.3-031903.201503261036_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-image-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
-
-# Remove 3.19.3
-sudo aptitude purge linux-headers-3.19.3-031903 linux-headers-3.19.3-031903-generic linux-image-3.19.3-031903-generic
-
 # http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000-generic_4.0.0-040000.201504121935_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000_4.0.0-040000.201504121935_all.deb
@@ -19,5 +11,4 @@ dpkg -l | grep "linux\-[a-z]*\-"
 sudo aptitude purge KERNEL_PACKAGES_TO_REMOVE
 
 # ref.
-#   https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1417263
 #   https://wiki.ubuntu.com/Kernel/MainlineBuilds?action=show&redirect=KernelMainlineBuilds
