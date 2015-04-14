061632f54ffc3476d98ab82c65984eba4dbb5bd9
ka <ka.kaosf@gmail.com>
2015-04-14 13:37:56 +0900

    Add download site URL to update-kernel.sh

--- update-kernel.sh
+++ update-kernel.sh
@@ -1,3 +1,4 @@
+# http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3.19.3-031903_3.19.3-031903.201503261036_all.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-image-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
