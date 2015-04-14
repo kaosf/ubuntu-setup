badc0d7bf9daceee20a309bbd4063d8c1ce2c336
ka <ka.kaosf@gmail.com>
2015-04-14 14:17:51 +0900

    Add a command to remove Linux kernel 3.19.3

--- update-kernel.sh
+++ update-kernel.sh
@@ -3,6 +3,9 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-image-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
 
+# Remove 3.19.3
+sudo aptitude purge linux-headers-3.19.3-031903 linux-headers-3.19.3-031903-generic linux-image-3.19.3-031903-generic
+
 # http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000-generic_4.0.0-040000.201504121935_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000_4.0.0-040000.201504121935_all.deb
