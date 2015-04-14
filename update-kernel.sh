9fc6520b56f04be6f4c9f8c33f90da543368efa7
ka <ka.kaosf@gmail.com>
2015-04-14 13:38:04 +0900

    Add commands note for Linux kernel 4.0

--- update-kernel.sh
+++ update-kernel.sh
@@ -3,6 +3,11 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-headers-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.3-vivid/linux-image-3.19.3-031903-generic_3.19.3-031903.201503261036_amd64.deb
 
+# http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000-generic_4.0.0-040000.201504121935_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000_4.0.0-040000.201504121935_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-image-4.0.0-040000-generic_4.0.0-040000.201504121935_amd64.deb
+
 sudo dpkg -i *.deb
 
 # ref.
