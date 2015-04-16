0a58f5394a7d1b9dd79127928aff84e9cd8c57a1
ka <ka.kaosf@gmail.com>
2015-04-16 18:48:49 +0900

    Fix vim-setup.sh
    Add Ruby installation note.
    Add development packages for X.

--- vim-setup.sh
+++ vim-setup.sh
@@ -21,11 +21,16 @@ git clone git://github.com/kaosf/ka-vim-snippets.git $HOME/.vim/ka-vim-snippets
 cd $HOME/.vim/bundle/vimproc
 make
 
+# Install Ruby before ./congiure
+rbenv install 2.2.2
+rbenv global 2.2.2
+
 ## Build by myself
 sudo aptitude install \
   lua5.2 liblua5.2-dev luajit libluajit-5.1-2 libluajit-5.1-dev \
   libperl-dev \
-  libpython-dev libpython3-dev
+  libpython-dev libpython3-dev \
+  libx11-dev libxt-dev libgtk2.0-dev
 mkdir -p $HOME/local/src
 cd $HOME/local/src
 # ref. https://github.com/vim-jp/vim
@@ -47,6 +52,11 @@ make
 make install
 
 # Without X
+sudo aptitude install \
+  lua5.2 liblua5.2-dev luajit libluajit-5.1-2 libluajit-5.1-dev \
+  libperl-dev \
+  libpython-dev libpython3-dev
+# ...
 ./configure \
   --prefix=$HOME/local \
   --enable-perlinterp=yes \
