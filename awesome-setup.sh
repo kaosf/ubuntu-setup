7d978118a719022e3d1c5ebf101fba6e70562f8e
ka <ka.kaosf@gmail.com>
2015-04-27 01:11:02 +0900

    Create awesome-setup.sh

---
+++ awesome-setup.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:klaus-vormweg/awesome
+sudo apt-get update
+sudo apt-get install awesome
+
+# ref.
+#   http://tanitta.hatenablog.com/entry/2014/11/25/010006
+#   http://projects.tsuntsun.net/~nabeken/diary/Awesome/tutorial-1.html
+#   http://awesome.naquadah.org/wiki/Awesome_3_configuration
+#   http://zentoo.hatenablog.com/entry/20100223/1266951112
