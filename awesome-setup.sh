7e283bdc51607aeb36739c172a4583b08ff1cb1a
ka <ka.kaosf@gmail.com>
2015-04-28 01:58:43 +0900

    Add a notes for awesome configurations

--- awesome-setup.sh
+++ awesome-setup.sh
@@ -2,6 +2,11 @@ sudo add-apt-repository ppa:klaus-vormweg/awesome
 sudo apt-get update
 sudo apt-get install awesome
 
+# Default configuration files
+mkdir -p $HOME/.config/awesome
+cp /etc/xdg/awesome/rc.lua $HOME/.config/awesome
+# Default theme: /usr/share/awesome/themes/default/theme.lua
+
 # ref.
 #   http://tanitta.hatenablog.com/entry/2014/11/25/010006
 #   http://projects.tsuntsun.net/~nabeken/diary/Awesome/tutorial-1.html
