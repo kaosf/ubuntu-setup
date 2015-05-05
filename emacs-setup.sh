5e5953fcbdf2445cbf08b4a92fad496d54f89bf6
ka <ka.kaosf@gmail.com>
2015-05-06 01:02:25 +0900

    Modify emacs-setup.sh
    Upgrade version to 24.5.
    Split commands to with-x and without-x.
    Add a command to remove .tar.gz.

--- emacs-setup.sh
+++ emacs-setup.sh
@@ -1,9 +1,18 @@
 #! /bin/sh
 
 cd $HOME/local/src
-wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-24.4.tar.gz
-tar xzf emacs-24.4.tar.gz
-cd emacs-24.4
+wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-24.5.tar.gz
+tar xzf emacs-24.5.tar.gz
+rm -f emacs-24.5.tar.gz
+cd emacs-24.5
+
+# With X
+sudo aptitude install libgif-dev libxpm-dev
+./configure --prefix=$HOME/local
+make
+make install
+
+# Without X
 ./configure --prefix=$HOME/local --without-x
 make
 make install
