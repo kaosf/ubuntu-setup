36bbfe5868e766b774906530a6dc41dcbf177108
ka <ka.kaosf@gmail.com>
2015-05-09 19:17:54 +0900

    Upgrade Tmux version from 1.9a to 2.0

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -3,10 +3,10 @@ sudo aptitude install libevent-dev libncurses-dev
 
 mkdir -p $HOME/local/src
 cd $HOME/local/src
-wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz
-tar xzf tmux-1.9a.tar.gz
-rm -f tmux-1.9a.tar.gz
-cd tmux-1.9a
+wget http://downloads.sourceforge.net/tmux/tmux-2.0.tar.gz
+tar xzf tmux-2.0.tar.gz
+rm -f tmux-2.0.tar.gz
+cd tmux-2.0
 ./configure --prefix=$HOME/local
 make
 make install
