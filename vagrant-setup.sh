a15927f0e7b3751b379544603dca13980a44cf13
ka <ka.kaosf@gmail.com>
2015-07-07 14:25:57 +0900

    Install vagrant-digitalocean plugin
    ref. http://blog.glidenote.com/blog/2013/12/05/digital-ocean-with-vagrant/

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -6,3 +6,5 @@ sudo dpkg -i 'virtualbox-4.3_4.3.14-95030~Ubuntu~raring_amd64.deb'
 # ref. http://www.vagrantup.com/downloads.html
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
 sudo dpkg -i vagrant_1.7.2_x86_64.deb
+# ref. http://blog.glidenote.com/blog/2013/12/05/digital-ocean-with-vagrant/
+vagrant plugin install vagrant-digitalocean
