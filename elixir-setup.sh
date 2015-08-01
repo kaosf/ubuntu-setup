31136daf0da0d04a63da8f8b2bcfafdbc43e9b04
ka <ka.kaosf@gmail.com>
2015-08-01 12:33:09 +0900

    Create elixir-setup.sh

---
+++ elixir-setup.sh
@@ -0,0 +1,8 @@
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update
+sudo apt-get install elixir
+
+# ref.
+#   http://elixir-lang.org/
+#   http://elixir-lang.org/getting-started/introduction.html
+#   http://elixir-lang.org/install.html#unix-and-unix-like
