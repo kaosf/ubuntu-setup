b9ba066a4b7cda45664f1e068d574d3e0f3e39e9
ka <ka.kaosf@gmail.com>
2015-08-16 03:16:52 +0900

    Update kernel version to 4.0.9

--- update-kernel.sh
+++ update-kernel.sh
@@ -1,7 +1,7 @@
-# http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.7-wily/
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.7-wily/linux-headers-4.0.7-040007-generic_4.0.7-040007.201507031036_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.7-wily/linux-headers-4.0.7-040007_4.0.7-040007.201507031036_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.7-wily/linux-image-4.0.7-040007-generic_4.0.7-040007.201507031036_amd64.deb
+# http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.9-wily/
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.9-wily/linux-headers-4.0.9-040009-generic_4.0.9-040009.201507212131_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.9-wily/linux-headers-4.0.9-040009_4.0.9-040009.201507212131_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.9-wily/linux-image-4.0.9-040009-generic_4.0.9-040009.201507212131_amd64.deb
 
 sudo dpkg -i *.deb
 
@@ -9,8 +9,6 @@ sudo dpkg -i *.deb
 dpkg -l | grep "linux\-[a-z]*\-"
 #sudo apt-get remove KERNEL_PACKAGES_TO_REMOVE
 #sudo aptitude purge KERNEL_PACKAGES_TO_REMOVE
-#sudo aptitude purge linux-headers-4.0.0-040000 linux-headers-4.0.0-040000-generic linux-image-4.0.0-040000-generic
-#sudo aptitude purge linux-headers-4.0.2-040002 linux-headers-4.0.2-040002-generic linux-image-4.0.2-040002-generic
 #sudo aptitude purge linux-headers-4.0.4-040004 linux-headers-4.0.4-040004-generic linux-image-4.0.4-040004-generic
 #sudo aptitude purge linux-headers-4.0.7-040007 linux-headers-4.0.7-040007-generic linux-image-4.0.7-040007-generic
 
