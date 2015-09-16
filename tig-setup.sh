0e63e6190569e0d8b75d36b7c9092858ef61224d
ka <ka.kaosf@gmail.com>
2015-09-16 22:52:01 +0900

    Fix tig and tmux setup commands

--- tig-setup.sh
+++ tig-setup.sh
@@ -1,7 +1,8 @@
-# ref. https://github.com/jonas/tig/issues/39#issuecomment-4992269
 sudo apt-get -y install automake libncurses5-dev libncursesw5-dev
+sudo apt-get -y remove tig
 mkdir -p $HOME/local/src
 cd $HOME/local/src
+rm -rf tig
 git clone https://github.com/jonas/tig
 cd tig
 git checkout tig-2.2.1
@@ -10,3 +11,6 @@ git checkout tig-2.2.1
 make
 make install
 git checkout master
+
+# ref. for dependent packages:
+#   https://github.com/jonas/tig/issues/39#issuecomment-4992269
--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,7 +1,8 @@
 sudo apt-get -y install libevent-dev libncurses-dev
-
+sudo apt-get -y remove tmux
 mkdir -p $HOME/local/src
 cd $HOME/local/src
+rm -rf tmux-*
 wget http://downloads.sourceforge.net/tmux/tmux-2.0.tar.gz
 tar xzf tmux-2.0.tar.gz
 rm -f tmux-2.0.tar.gz
