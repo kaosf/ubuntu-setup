8c4dfbb18df73699bd036205db93ef3b59960735
ka <ka.kaosf@gmail.com>
2015-10-02 02:23:24 +0900

    Use wget instead of curl on Ubuntu

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -1,7 +1,8 @@
 # install Node.js with nodebrew
 # ref. https://github.com/hokaccha/nodebrew
 
-curl -L git.io/nodebrew | perl - setup
+# curl -L git.io/nodebrew | perl - setup
+wget git.io/nodebrew -O - | perl - setup
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> $HOME/.zshenv
 export PATH=$HOME/.nodebrew/current/bin:$PATH
 
