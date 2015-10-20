95fe37114ffc8f8a7e1928573f8957a074261c9c
ka <ka.kaosf@gmail.com>
2015-10-20 20:08:07 +0900

    Bump tmux version to 2.1

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,13 +1,14 @@
+VERSION=2.1
 if [[ $1 = local ]]; then
   echo 'Build "libevent-dev" and "libncurses-dev".' >&2
 else
   sudo apt-get -y install libevent-dev libncurses-dev
   sudo apt-get -y remove tmux
 fi
-wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
-tar xzf tmux-2.0.tar.gz
-rm -f tmux-2.0.tar.gz
-cd tmux-2.0
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xzf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
 if [[ $1 = local ]]; then
   ./configure --prefix=$HOME/local
   make
@@ -15,12 +16,12 @@ if [[ $1 = local ]]; then
   mkdir -p $HOME/local/src
   cd -
   rm -rf $HOME/local/src/tmux-*
-  mv tmux-2.0 $HOME/local/src
+  mv tmux-${VERSION} $HOME/local/src
 else
   ./configure
   make
   sudo make install
   cd -
   sudo rm -rf /usr/local/src/tmux-*
-  sudo mv tmux-2.0 /usr/local/src
+  sudo mv tmux-${VERSION} /usr/local/src
 fi
