4e46f52856c13c473139e31e3fda46fdf8c22c82
ka <ka.kaosf@gmail.com>
2015-10-27 21:07:15 +0900

    Create unsecure-root-setup.sh

---
+++ unsecure-root-setup.sh
@@ -0,0 +1,12 @@
+if [ $# -lt 1 ]; then exit 1; fi
+NAME=$1
+groupadd $NAME
+cp -r /etc/skel /home/$NAME
+useradd $NAME -g $NAME -s /bin/bash -d /home/$NAME
+echo "${NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+mkdir /home/${NAME}/.ssh
+cp .ssh/authorized_keys /home/${NAME}/.ssh/
+chown ${NAME}:${NAME} -R /home/${NAME}
+chmod 600 /home/${NAME}/.ssh/authorized_keys
+sed 's/PermitRootLogin yes/PermitRootLogin no/' -i /etc/ssh/sshd_config
+service ssh restart
