98addc9bac3e105c2c330fc302ca9a96342919aa
ka <ka.kaosf@gmail.com>
2017-01-20 21:32:42 +0900

    Remove dgate-setup.sh

--- dgate-setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p $HOME/local/src
-cd $HOME/local/src
-wget https://deploygate.com/client/dgate.tar.gz
-mkdir -p $HOME/local/opt/dgate
-cd $HOME/local/opt/dgate
-tar xzf $HOME/local/src/dgate.tar.gz
-echo 'export PATH=$HOME/local/opt/dgate:$PATH' >> $HOME/.zshenv
