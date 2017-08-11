1fdbb7f859830b46188afaef00402cb3aaa6691e
ka <ka.kaosf@gmail.com>
2017-08-11 13:55:17 +0900

    Create ffmpeg-setup.sh

---
+++ ffmpeg-setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+# Press ENTER to continue
+
+sudo apt-get -y install ffmpeg
+# ref. http://d.hatena.ne.jp/katz_24/20160323/1458784199
