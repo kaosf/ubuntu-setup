404ea374f81d35aa581037973bdd22fbd7d598ce
ka <ka.kaosf@gmail.com>
2017-10-21 16:09:06 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,12 +1,4 @@
-# ref. https://docs.docker.com/engine/installation/linux/ubuntulinux/
-sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv \
-  --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get -y install docker-engine
+# Refer here: https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu
 
 sudo systemctl start docker.service
 
