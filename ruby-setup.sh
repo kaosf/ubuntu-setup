95c1f823407a1f91c634d984cd5800f553bc708a
ka <ka.kaosf@gmail.com>
2017-11-14 19:12:49 +0900

    Update ruby-setup.sh

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -7,19 +7,16 @@
 
 # zlib1g-dev libssl-dev libreadline6-dev are needed to build
 # libsqlite3-dev for SQLite3
-# postgresql-server-dev-9.3 for PostgreSQL 9.3
 # libxslt1-dev libxml2-dev for nokogiri
 sudo apt-get -y install zlib1g-dev libssl-dev libreadline6-dev libffi-dev \
-  libsqlite3-dev \
-  postgresql-server-dev-9.3 \
-  libxslt1-dev libxml2-dev
+  libsqlite3-dev libxslt1-dev libxml2-dev
 
 git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.profile
-echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv
-echo 'eval "$(rbenv init -)"' >> $HOME/.profile
-exec $SHELL -l
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bashrc
+echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
 mkdir -p $HOME/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 git clone https://github.com/ianheggie/rbenv-binstubs.git $HOME/.rbenv/plugins/rbenv-binstubs
@@ -29,6 +26,10 @@ rbenv rehash
 gem install bundler --no-ri --no-rdoc
 rbenv rehash
 
+# For PostgreSQL
+sudo apt-get -y install postgresql-server-dev-all
+# For MySQL
+sudo apt-get -y install libmysqlclient-dev
 # For ruby-ldap
 # ref. https://github.com/bearded/ruby-ldap/issues/7
 sudo apt-get -y install libldap2-dev libsasl2-dev
@@ -39,3 +40,9 @@ PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig \
   gem install rmagick -v 2.13.3
 # For filemagic
 sudo apt-get -y install libmagic-dev
+
+# For zsh
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshenv
+echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.profile
+echo 'eval "$(rbenv init -)"' >> $HOME/.profile
