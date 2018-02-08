e797d6b14062f93c3f784bbc93239b89e7bb9ae6
ka <ka.kaosf@gmail.com>
2018-02-08 21:05:34 +0900

    Upgrade docker-compose version to 1.19.0

--- docker-compose-setup.sh
+++ docker-compose-setup.sh
@@ -1,3 +1,3 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # ref. https://docs.docker.com/compose/install/#install-compose
