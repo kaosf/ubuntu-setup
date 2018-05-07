30d243b0d86d8191d51885d6fa1d5c3b45d4a4e6
ka <ka.kaosf@gmail.com>
2018-05-07 16:30:05 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,4 +1,12 @@
-# Refer here: https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu
+# Refer here: https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+# Installation END
 
 sudo systemctl start docker.service
 
