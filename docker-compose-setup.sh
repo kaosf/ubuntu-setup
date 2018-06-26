95fbad0bb29622ebe6d4b368b99d7c15666073ad
ka <ka.kaosf@gmail.com>
2018-06-26 09:37:04 +0900

    Update docker-compose-setup.sh

--- docker-compose-setup.sh
+++ docker-compose-setup.sh
@@ -1,3 +1,3 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # ref. https://docs.docker.com/compose/install/#install-compose
