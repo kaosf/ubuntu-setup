b55b2b5b8a4e8c4724d90f2b7c3270ec942c05e0
ka <ka.kaosf@gmail.com>
2019-05-02 12:52:49 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,11 +1,11 @@
 # Refer here: https://docs.docker.com/install/linux/docker-ce/ubuntu/
 sudo apt-get update
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 # Installation END
 
 # Ubuntu 18.04 on ConoHa
