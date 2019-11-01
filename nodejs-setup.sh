75584810526496bc9d809ec2d7f45bf138b9ef2e
ka <ka.kaosf@gmail.com>
2019-11-01 13:34:16 +0900

    Update Node.js setup notes

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -1,13 +1,3 @@
-# install Node.js with nodebrew
-# ref. https://github.com/hokaccha/nodebrew
-
-# curl -L git.io/nodebrew | perl - setup
-wget git.io/nodebrew -O - | perl - setup
-echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> $HOME/.zshenv
-echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> $HOME/.profile
-export PATH=$HOME/.nodebrew/current/bin:$PATH
-
-# nodebrew help
-# nodebrew ls-all
-nodebrew install-binary 8.4.0
-nodebrew use latest
+sudo apt-get -y install nodejs npm
+sudo npm i -g n
+sudo n v12.11.0
