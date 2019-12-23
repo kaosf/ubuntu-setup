2ea6d72c4dba7d2455d5aca06c97e4ce983a954c
ka <ka.kaosf@gmail.com>
2019-12-23 15:59:43 +0900

    Update docker-compose version

--- docker-setup.sh
+++ docker-setup.sh
@@ -42,7 +42,7 @@ sudo systemctl restart docker.service
 
 ###
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # ref. https://docs.docker.com/compose/install/#install-compose
 
